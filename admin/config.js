df4af237f72b896a8293bcf746712597fb669a73
BloxxStar <kontakt@bloxxstar.de>
2026-07-20 15:03:33 +0200

    chore: Supabase-Projekt für Admin-Backend konfigurieren

--- admin/config.js
+++ admin/config.js
@@ -1,4 +1,4 @@
 window.BLOXXSTAR_SUPABASE = {
-  url: "",
-  publishableKey: ""
+  url: "https://grfyhxqrlosztuinrpvp.supabase.co",
+  publishableKey: "sb_publishable_4UDF-5F42rubjb1Y2dnbog_4hlCq2oO"
 };
